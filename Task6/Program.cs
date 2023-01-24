3dc2ba1365a93f4d00002883ed460229155e41d1
Kamila <kamilla.adamson@gmail.com>
2023-01-24 20:14:49 +0600

    added a task6

---
+++ Task6/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит
+//  расстояние между ними в 3D пространстве.
+
+Console.Clear();
+
+Console.WriteLine("Введите координату X для точки A: ");
+int xA = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координату Y для точки A: ");
+int yA = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координату Z для точки A: ");
+int zA = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координату X для точки B: ");
+int xB = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координату Y для точки B: ");
+int yB = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координату Z для точки B: ");
+int zB = int.Parse(Console.ReadLine()!);
+
+// result = √(xA-xB)²+(yA-yB)²+(zA-zB)²
+
+double result = Math.Sqrt((Math.Pow((xA - xB),2) + Math.Pow((yA - yB),2) + Math.Pow((zA - zB),2)));
+
+Console.WriteLine($"Расстояние между заданными точками {result:f2}");
+
+
