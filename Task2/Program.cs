438a948e3fb177944e7c4204c07ebab2362d089f
Kamila <kamilla.adamson@gmail.com>
2023-01-24 19:40:40 +0600

    added a task2

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет. (Цифры считать справа налево).
+
+Console.Clear();
+
+Console.WriteLine("Введите трёхзначное число");
+
+
+//int i = int.MinValue && int.MaxValue
+
+int i = new Random().Next(int.MinValue, int.MaxValue);
+
+
+if ((i / 100) != 0)
+{ 
+    int ix = (i / 100) % 10;
+    Console.WriteLine($"третья цифра числа {i} -> {ix}");
+} 
+else 
+{
+    Console.WriteLine($"число {i} не имеет третьего числа");
+}
+
