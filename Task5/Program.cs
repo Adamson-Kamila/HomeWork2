070b76b105682b5d1a9966be87c6bfbe738a7629
Kamila <kamilla.adamson@gmail.com>
2023-01-24 20:12:58 +0600

    added a task5

---
+++ Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int count = 1;
+Console.Write($"{Math.Pow(count,3)}");
+count++;
+while(count < number+1) 
+{
+   double result = Math.Pow(count,3);
+   Console.Write($", {result}");
+   count++;
+}
