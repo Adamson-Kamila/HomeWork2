b7be37cf6ee66b3b993458d90153a6fe779f43ef
Kamila <kamilla.adamson@gmail.com>
2023-01-24 20:11:59 +0600

    added a task4

---
+++ Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+
+Console.Clear();
+
+ Console.WriteLine("Введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+
+
+int a = num / 100 / 100;
+int b = num / 100 / 10 % 10;
+int c = num / 100 % 10;
+int d = num % 100 / 10;
+int e = num % 10;
+
+if (a == e) 
+{
+    if (b == d)
+    Console.WriteLine($"Число {num} палиндром. ");
+} else 
+{
+    Console.WriteLine($"Число {num} не является палиндромом. ");
+}
+
+
